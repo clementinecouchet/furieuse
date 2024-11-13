8006a91224d48abe0c78ed13a079c0fe21d8483c
clementinecouchet <clementine.couchet@studentfr.ch>
2024-11-13 08:53:21 +0100

    html css js du premier slider scroll honrizontal

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".slider-container", {
+  x: "-76%", // chaque élément .slider-item vaut 20% de .slider-container, on veut donc bouger la position x de -76% pour faire apparaitre le dernier élément: chaque élément a besoin de 20% pour apparaitre pleinement sur l'écran.
+  scrollTrigger: {
+    trigger: ".slider-mask",
+    start: "top top",
+    scrub: 1,
+    pin: true,
+  },
+});
