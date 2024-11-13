bf69cc6e3417141ad27423b80cf48b8b826a678e
clementinecouchet <clementine.couchet@studentfr.ch>
2024-11-13 11:46:18 +0100

    hover scroll + spacer

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.to(".slider-container", {
-  x: "-82.5%", // chaque élément .slider-item vaut 20% de .slider-container, on veut donc bouger la position x de -76% pour faire apparaitre le dernier élément: chaque élément a besoin de 20% pour apparaitre pleinement sur l'écran.
+  x: "-81.5%", // chaque élément .slider-item vaut 20% de .slider-container, on veut donc bouger la position x de -76% pour faire apparaitre le dernier élément: chaque élément a besoin de 20% pour apparaitre pleinement sur l'écran.
   scrollTrigger: {
     trigger: ".slider-mask",
     start: "top top",
