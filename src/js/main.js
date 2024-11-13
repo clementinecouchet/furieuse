7fd7d16131433fb176338da1cb25afc65ddec89c
clementinecouchet <clementine.couchet@studentfr.ch>
2024-11-13 14:55:03 +0100

    wrapper-bulles html, css, js

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,50 @@ gsap.to(".slider-container", {
     pin: true,
   },
 });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".container-wrap",
+      start: "top top",
+      scrub: true,
+      pin: true,
+    },
+  })
+  .to("#bulle-1", {
+    //La position que la bulle va prendre, c'est mieux de mettre en pourcentage afin de garantir une meilleure responsivité
+    left: "10%",
+    top: "1%",
+    opacity: 1, // rendre visible
+  })
+  .to("#bulle-2", {
+    left: "60%",
+    top: "19%",
+    opacity: 1,
+  })
+  .to("#bulle-3", {
+    left: "10%",
+    top: "35%",
+
+    opacity: 1,
+  })
+  .to("#bulle-4", {
+    left: "5%",
+    top: "60%",
+    opacity: 1,
+  })
+  .to("#bulle-5", {
+    left: "60%",
+    top: "60%",
+    opacity: 1,
+  })
+  .to("#bulle-6", {
+    left: "50%",
+    top: "8%",
+    opacity: 1,
+  })
+  .to("#bulle-7", {
+    left: "20%",
+    top: "55%",
+    opacity: 1,
+  });
